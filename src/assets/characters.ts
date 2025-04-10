93324453dc9e4b0c2c54118ede2a576081464bfa
A-M98 <aalmohammad@pifss.gov.kw>
2025-04-10 11:00:01 +0300

    Added remaingin images and achievments

--- src/assets/characters.ts
+++ src/assets/characters.ts
@@ -11,7 +11,7 @@ const characters: Characters[] = [
   {
     id: 1,
     name: 'Captain Price',
-    img: 'captain-price.png',
+    img: './assets/captain-price.png',
     damage: 10,
     timer: 20,
     ability: 'Doubles your clicks',
@@ -19,7 +19,7 @@ const characters: Characters[] = [
   {
     id: 2,
     name: 'Lana Del Ray',
-    img: 'lana.png',
+    img: './assets/Lana-Del-Rey.png',
     damage: 20,
     timer: 33,
     ability: 'Enables god rays',
@@ -27,7 +27,7 @@ const characters: Characters[] = [
   {
     id: 3,
     name: 'Mohammed Hunaidi',
-    img: 'hunaidi.png',
+    img: './assets/laughing.png',
     damage: 40,
     timer: 10,
     ability: 'Shows laughing gif',
@@ -35,7 +35,7 @@ const characters: Characters[] = [
   {
     id: 4,
     name: 'Mishari',
-    img: 'mishari.png',
+    img: './assets/mishari.png',
     damage: 77,
     timer: 10,
     ability: 'Gives you fruits',
@@ -43,7 +43,7 @@ const characters: Characters[] = [
   {
     id: 5,
     name: '7moodi',
-    img: '7moodi.png',
+    img: './assets/7moodi.png',
     damage: 150,
     timer: 10,
     ability: 'Gives you motivation',
@@ -51,7 +51,7 @@ const characters: Characters[] = [
   {
     id: 6,
     name: 'Snowy',
-    img: 'snowy.png',
+    img: './assets/White_Persian_Cat-removebg-preview.png',
     damage: 200,
     timer: 15,
     ability: 'Sleeps next to you',
@@ -59,7 +59,7 @@ const characters: Characters[] = [
   {
     id: 7,
     name: 'Al6af',
-    img: 'al6af.png',
+    img: './assets/al6af.png',
     damage: 450,
     timer: 10,
     ability: 'Disables click button',
@@ -67,7 +67,7 @@ const characters: Characters[] = [
   {
     id: 8,
     name: 'Undertaker',
-    img: 'undertaker.png',
+    img: './assets/undertaker.png',
     damage: 750,
     timer: 10,
     ability: "Kills Mishari if they're available at the same time",
@@ -75,7 +75,7 @@ const characters: Characters[] = [
   {
     id: 9,
     name: 'Captain Majed',
-    img: 'majed.png',
+    img: './assets/tsubasa.png',
     damage: 1000,
     timer: 10,
     ability: 'Does nothing for now…',
